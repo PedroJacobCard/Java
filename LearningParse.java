638a09fdc57b7937c840ea03c53b322c293b1539
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2023-04-14 09:22:54 -0300

    ja

--- LearningParse.java
+++ LearningParse.java
@@ -1,9 +1,10 @@
 public class LearningParse {
     public static void main(String enter[]){
         int n1, n2, soma;
-        n1 = integer.parseInt(enter[0]);
-        n2 = integer.parseInt(enter[1]);
+        n1 = Integer.parseInt(enter[0]);
+        n2 = Integer.parseInt(enter[1]);
         soma = n1 + n2;
         System.out.println(n1 + " + " + n2 + " = " + soma);
+        System.exit(0);
     }
 }
