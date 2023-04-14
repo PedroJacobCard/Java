cfd999e910234394e8691062e176abf37f83cb4a
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2023-04-14 09:04:35 -0300

    ja

--- TrainingVar.java
+++ TrainingVar.java
@@ -4,7 +4,7 @@ public class TrainingVar {
         int age = 22;
         double weight = 74.50;
         double height = 1.71;
-        char gender = M;
+        char gender = m;
 
         System.out.println("The person is " + gender + " the name is: " + name + ", and has " + age + " years old. " + name + "'s weight is " + weight + " and is " + height + " tall.");
     }
