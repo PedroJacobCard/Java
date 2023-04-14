36885428054331835a9c90f778da9cacc36efab2
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2023-04-14 09:18:39 -0300

    commiting new file

---
+++ LearningParse.java
@@ -0,0 +1,9 @@
+public class LearningParse {
+    public static void main(String enter[]){
+        int n1, n2, soma;
+        n1 = integer.parseInt(enter[0]);
+        n2 = integer.parseInt(enter[1]);
+        soma = n1 + n2;
+        System.out.println(soma);
+    }
+}
