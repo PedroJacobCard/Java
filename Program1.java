6bf8c1c1bc358b84a69fe0ba2cb4fab7b1ed5fef
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2023-04-28 09:05:45 -0300

    new Java program file

---
+++ Program1.java
@@ -0,0 +1,21 @@
+public class Program1 {
+    public static void main(String enter[]){
+        int numInt;
+        double numReal, calc;
+        char chart;
+
+        numInt = Integer.parseInt(enter[0]);
+        numReal = Double.parseDouble(enter[1]);
+        chart = (enter[2]).charAt(0);
+
+        switch(chart){
+            case '+': calc = (double)numInt + numReal;
+            case '-': calc =(double)numInt - numReal;
+            case '*': calc =(double)numInt * numReal;
+            case '/': calc =(double)numInt / numReal;
+            System.out.println("The calc is " + (double)numInt + chart + numReal + " = " + calc + " singnal " + chart);
+        }
+        //soma = (double)numInt + numReal;
+        System.exit(0);
+    }
+}
